aa8adbdb3a9344703476c465ee1798f2906703df
iamzeroo <rmpostma@hotmail.com>
2016-09-05 17:37:52 +0100

    Initial Commit

---
+++ Nobody Knows/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nobody Knows
+//
+//  Created by Renze Postma on 05/09/2016.
+//  Copyright © 2016 Renze Postma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
